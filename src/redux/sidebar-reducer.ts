1e3cdfe89ded5f5c712f19315a94303a0bbbb3cf
Inna Groznaya <inna2011@tut.by>
2023-08-29 16:36:07 +0300

    Updates

--- src/redux/sidebar-reducer.ts
+++
@@ -1,6 +0,0 @@
-import {ActionTypes, SidebarType, StateType} from "./store";
-
-let initialState = {}
-export const sidebarReducer = (state: SidebarType = initialState, action: ActionTypes) => {
-    return state
-}
--- src/utils/object-helpers.ts
+++
@@ -1,5 +0,0 @@
-export const updateObjectInArray = (items: Array<any>, itemId: number, objPropName: string, newObjProps: any) => {
-    return items.map(u =>
-          u[objPropName] === itemId ? { ...u, ...newObjProps} : u
-        )
-} 
